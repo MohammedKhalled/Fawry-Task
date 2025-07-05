5038281c1349b9cbc3cac9bc6442e41c9dd060c9
Mohamed Khaled <mhmddkhaled618@gmail.com>
2025-07-05 03:26:09 +0300

    feat: Add product model classes

---
+++ src/Model/RegularProduct.java
@@ -0,0 +1,8 @@
+package Model;
+
+
+public class RegularProduct extends Product{
+    public RegularProduct(String name, double price, int quantity) {
+        super(name, price, quantity);
+    }
+}
