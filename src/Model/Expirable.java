ce123ecab7eb6e6fa6d3b0a7acf6a8f51f186803
Mohamed Khaled <mhmddkhaled618@gmail.com>
2025-07-05 03:09:48 +0300

    feat: product model

---
+++ src/Model/Expirable.java
@@ -0,0 +1,5 @@
+package Model;
+
+public interface Expirable {
+    boolean isExpired();
+}
---
+++ src/Model/Shippable.java
@@ -0,0 +1,6 @@
+package Model;
+
+public interface Shippable {
+    String getName();
+    double getWeight();
+}
